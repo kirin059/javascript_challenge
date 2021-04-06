561242b099d1f993a77744cc04b07b3989e60c55
saebom <boma91@gmail.com>
2021-04-06 21:17:55 +0900

    Add: calculator 수정

--- calculator/index.js
+++ calculator/index.js
@@ -1,54 +1,66 @@
 'use strict';
 
-const operator = document.querySelectorAll(".operator"); // 연산자
-const numBtn = document.querySelectorAll(".number"); // 숫자(0~9)
-const input = document.querySelector(".input"); // 입력란
+// 1) 버튼 입력 기능
+// 2) 연산 기능  >> 식과 값 input 분리하기
+// 3) AC 기능
+// 4) del 기능
 
+const num = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".cal")
 
-const previous = document.querySelector(".previous");
-const current = document.querySelector(".current");
+const next = document.querySelector(".next");
+const pre = document.querySelector(".previous");
 
-
-// 숫자 및 연산자는 "배열"로 받아진다(querySelectorAll)
-
-// 버튼 클릭 시, 입력창에 입력 기능
+// 숫자버튼+연산버튼 누르면 input에 입력되도록 하는 함수
 const btnClick = () => {
-    for (let i = 0; i < numBtn.length; i++) {
-        numBtn[i].addEventListener("click", function (e) {
-            //console.log(e.target);
-            current.value = (current.value += e.target.value).toLocaleString();
-        });
+    for (let i = 0; i < num.length; i++) {
+        num[i].addEventListener('click', e => {
+            // console.log(e.currentTarget)
+            next.value = (next.value += e.target.value).toLocaleString();
+        })
     }
 
     for (let i = 0; i < operator.length; i++) {
-        operator[i].addEventListener("click", function (e) {
-            //console.log(e.target);
-            current.value += e.target.value;
-        });
+        operator[i].addEventListener('click', e => {
+            next.value = (next.value += e.target.value)
+        })
     }
-};
-btnClick();
+}
+btnClick()
+
 
 // 연산 기능
-const resultBtn = document.querySelector(".result");
+const result = document.querySelector('.result');
 
-resultBtn.addEventListener("click", (e) => {
-    previous.value = current.value;
-    current.value = eval(current.value).toLocaleString();
-});
+const resultNumber = () => {
+    result.addEventListener('click', e => {
+        // console.log(e.target)
+        pre.value = next.value;
+        next.value = eval(next.value).toLocaleString();
+    })
+}
+resultNumber()
 
-// 초기화(AC) 기능
-const reset = document.querySelector(".clear");
 
-reset.addEventListener("click", (e) => {
-    previous.value = "";
-    current.value = "";
-});
 
-// delete 기능
-const del = document.querySelector(".del");
+// 초기화(AC)기능
+const reset = document.querySelector('.clear')
+// console.log(reset)
 
-del.addEventListener("click", (e) => {
-    current.value = current.value.slice(btnClick.length, -1);
-});
+const resetAll = () => {
+    reset.addEventListener('click', e => {
+        pre.value = '';
+        next.value = '';
+    })
+}
+resetAll()
 
+// 삭제(del) 기능
+const del = document.querySelector('.del');
+
+const deleteValue = () => {
+    del.addEventListener('click', e => {
+        next.value = next.value.slice(btnClick.length, -1)
+    })
+}
+deleteValue()
